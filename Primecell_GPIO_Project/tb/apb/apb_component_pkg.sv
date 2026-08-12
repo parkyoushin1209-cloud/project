91baa1ca9b21775c409dd2ce7caefd1a9c80f6b0
parkyoushin1209-cloud <parkyoushin1209@gmail.com>
2026-08-12 14:28:59 +0900

    Create apb_component_pkg.sv

---
+++ Primecell_GPIO_Project/tb/apb/apb_component_pkg.sv
@@ -0,0 +1,14 @@
+package apb_component_pkg;
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+
+
+`include "apb_driver_t.sv"
+`include "apb_monitor_t.sv"
+`include "apb_sequencer_t.sv"
+`include "apb_cov_coll_t.sv"
+
+`include "apb_agent_t.sv"
+`include "apb_env_t.sv"
+endpackage : apb_component_pkg
