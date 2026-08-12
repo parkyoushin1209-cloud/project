c5b80feec53eb92aed2b1fa769a61fe5a46ded66
parkyoushin1209-cloud <parkyoushin1209@gmail.com>
2026-08-12 14:50:39 +0900

    Create apb_sequence_pkg.sv

---
+++ Primecell_GPIO_Project/sequences/apb/apb_sequence_pkg.sv
@@ -0,0 +1,21 @@
+package apb_sequence_pkg;
+`include "apb_base_seq_t.sv"
+
+`include "apb_seq_t.sv"
+`include "apb_reg_seq_t.sv"
+
+`include "apb_ie_reg_seq_t.sv"
+`include "apb_is_reg_seq_t.sv"
+`include "apb_ic_reg_seq_t.sv"
+`include "apb_periphid_reg_seq_t.sv"
+`include "apb_pcellid_reg_seq_t.sv"
+`include "apb_af_sel_reg_seq_t.sv"
+`include "apb_data_reg_seq_t.sv"
+`include "apb_dir_reg_seq_t.sv"
+`include "apb_ibe_reg_seq_t.sv"
+`include "apb_iev_reg_seq_t.sv"
+`include "apb_mis_reg_seq_t.sv"
+`include "apb_ris_reg_seq_t.sv"
+
+`include "apb_reg_lib_seq_t.sv"
+endpackage : apb_sequence_pkg
