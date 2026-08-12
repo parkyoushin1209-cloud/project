82aead4ce5fbedb349582dd5cbba6764d32416f8
parkyoushin1209-cloud <parkyoushin1209@gmail.com>
2026-08-12 14:41:30 +0900

    Create apb_data_reg_seq_t.sv

---
+++ Primecell_GPIO_Project/sequences/apb/apb_data_reg_seq_t.sv
@@ -0,0 +1,20 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+class apb_data_reg_seq_t extends apb_base_seq_t;
+`uvm_object_utils(apb_data_reg_seq_t)
+
+function new(string name  = "apb_data_reg_seq_t");
+super.new(name);
+endfunction : new
+
+virtual task body();
+start_item(apb_tr);
+if(!apb_tr.randomize() with {
+    apb_tr.addr[11:0] inside {[12'h000 : 12'h3FF]};
+    apb_tr.addr[31:12] == 20'h00000;
+     }) `uvm_error("RANDFAIL", "Failed in randomize")
+finish_item(apb_tr);
+endtask : body
+
+endclass : apb_data_reg_seq_t
