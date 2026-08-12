24c61cc418a9f8530150827cb30aed53801ebd57
parkyoushin1209-cloud <parkyoushin1209@gmail.com>
2026-08-12 14:35:51 +0900

    Create gpio_component_pkg.sv

---
+++ Primecell_GPIO_Project/tb/gpio/gpio_component_pkg.sv
@@ -0,0 +1,22 @@
+
+package gpio_component_pkg;
+
+
+`include "gpio_driver_t.sv"
+`include "gpio_sequencer_t.sv"
+`include "gpio_monitor_t.sv"
+`include "gpio_io_monitor_t.sv"
+
+`include "gpio_agent_t.sv"
+`include "gpio_io_agent_t.sv"
+
+`include "gpio_scoreboard_t.sv"
+`include "vsequencer_t.sv"
+`include "gpio_cov_coll_t.sv"
+
+`include "sqr_aggregator_t.sv"
+`include "gpio_env_t.sv"
+
+`include "gpio_system_env_t.sv"
+
+endpackage : gpio_component_pkg
