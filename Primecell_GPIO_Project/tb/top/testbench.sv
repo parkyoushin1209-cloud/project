d5c3c410ec18a041fa6f36b0589bb241c24a2937
parkyoushin1209-cloud <parkyoushin1209@gmail.com>
2026-08-12 14:08:38 +0900

    Create testbench.sv

---
+++ Primecell_GPIO_Project/tb/top/testbench.sv
@@ -0,0 +1,25 @@
+`include "apb_if.sv"
+`include "gpio_core_if.sv"
+`include "gpio_io_if.sv"
+`include "reset_if.sv"
+
+`include "design.sv"
+
+`include "apb_checker_t.sv"
+`include "gpio_checker_t.sv"
+
+`include "apb_transaction_pkg.sv"
+`include "gpio_transaction_pkg.sv"
+`include "gpio_reg_model_pkg.sv"
+
+`include "apb_component_pkg.sv"
+`include "gpio_component_pkg.sv"
+
+`include "apb_sequence_pkg.sv"
+`include "gpio_sequence_pkg.sv"
+
+`include "virtual_sequence_pkg.sv"
+
+`include "gpio_test_pkg.sv"
+
+`include "tb_top.sv"
